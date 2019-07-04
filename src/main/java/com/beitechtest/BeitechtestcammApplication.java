82bad0ec098f449f630fa8d30065a818648aff6b
Carlos Maturana <caloja.sun@gmail.com>
2019-07-04 12:40:33 -0500

    First Load

---
+++ src/main/java/com/beitechtest/BeitechtestcammApplication.java
@@ -0,0 +1,13 @@
+package com.beitechtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeitechtestcammApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeitechtestcammApplication.class, args);
+	}
+
+}
